0eac24dc0d147ff9e4d7c285e7088ac1d73690bb
Adrian-Garcia-Cuya <adrian123575@outlook.com>
2023-03-22 13:26:26 -0500

    se agrego un formulario y una ventana emergente

--- lib/converter.dart
+++ lib/converter.dart
@@ -8,11 +8,76 @@ class Converter extends StatefulWidget {
 }
 
 class _ConverterState extends State<Converter> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Center(
         child: ListView(
-      children: const [Text('data')],
+      children: [
+        Column(
+          children: [
+            Form(
+              key: _formKey,
+              child: TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, ingrese algún texto.';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    showDialog<String>(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        icon: const Icon(
+                          Icons.check_circle,
+                          size: 50,
+                          color: Color(0xFF54B435),
+                          shadows: [
+                            BoxShadow(
+                              color: Color(0xFF379237),
+                              blurRadius: 5.0,
+                            )
+                          ],
+                        ),
+                        content: const Text('Los Conversión a sido exitosa'),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () => Navigator.pop(context, 'OK'),
+                            child: const Text('OK'),
+                          ),
+                        ],
+                      ),
+                    );
+                  }
+                },
+                child: const Text('Enviar'))
+          ],
+        ),
+      ],
     ));
   }
 }
+
+class DialogExample extends StatelessWidget {
+  const DialogExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: const Text('Show Dialog'),
+    );
+  }
+}
+
+// if (_formKey.currentState!.validate()) {
+//                     ScaffoldMessenger.of(context).showSnackBar(
+//                       const SnackBar(content: Text('Registrando datos...')),
+//                     );
+//                   }
